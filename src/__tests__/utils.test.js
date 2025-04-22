1e0d1af46ebc66c7830a6cf8c66be7e00d9e5eec
Zacharia Ndinguri <zacthuku@gmail.com>
2025-04-22 13:00:47 +0300

    tdd writing test lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,19 @@
 // Your tests here
+import { pointsForWord } from "../utils";
+
+describe("pointsForWord", () => {
+  it("calculates the total points for a word (1 point per vowel, 2 per consonant)", () => {
+    const word = "test";
+
+    const points = pointsForWord(word);
+
+    expect(points).toBe(7);
+  });
+  it("handles uppercase and lowercase input", () => {
+    const word = "tEsT";
+  
+    const points = pointsForWord(word);
+  
+    expect(points).toBe(7);
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,9 @@
 // Your code here
+
+  export function pointsForWord(word) {
+    let points = 0;
+    for (const char of word) {
+      points += /[aeiou]/i.test(char) ? 1 : 2;
+    }
+    return points;
+  }
